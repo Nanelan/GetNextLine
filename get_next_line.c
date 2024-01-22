bf61a66356ad2b216976598ba60b0aab7b5087b9
Nanelan <151051891+Nanelan@users.noreply.github.com>
2024-01-22 17:31:25 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: crmunoz- <crmunoz-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 13:51:31 by crmunoz-          #+#    #+#             */
-/*   Updated: 2024/01/22 13:03:43 by crmunoz-         ###   ########.fr       */
+/*   Updated: 2024/01/22 17:30:15 by crmunoz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,15 @@ char	*ft_free_line(char *text)
 
 	i = 0;
 	j = 0;
-	temptext = NULL;
 	while (text[i] != '\n')
 		i++;
+	temptext = malloc((sizeof(char) * i) + 1);
 	while (text[i] != '\0')
 	{
 		temptext[j] = text[i + j];
 		j++;
 	}
+	temptext[i + j] = '\0';
 	free(text);
 	return (temptext);
 }
@@ -38,38 +39,40 @@ char	*get_next_line(int fd)
 	static char	*text;
 	int			bytesread;
 	int			i;
+	int			j;
 	char		*line;
-
-	text = NULL;
+	
 	line = NULL;
 	i = 0;
+	j = 0;
 	bytesread = read(fd, buffer, BUFFER_SIZE);
 	buffer[BUFFER_SIZE] = '\0';
 	if (bytesread > 0)
 	{
-		while (buffer[i] != '\n' || buffer[i] == '\0')
+		while (buffer[j] != '\0')
 		{
-			text = ft_strjoin(text, buffer);
-			i++;
-		}
-		if (ft_strchr(text, '\n') == NULL)
-			get_next_line(fd);
-		else if (*ft_strchr(text, '\n') == '\n')
-		{
-			i = 0;
-			while (text[i] != '\n')
+			if (*buffer != '\n' || *buffer == '\0')
+				text = ft_strjoin(text, buffer);
+			if (ft_strchr(text, '\n') == NULL)
+				get_next_line(fd);
+			else if (*ft_strchr(text, '\n') == '\n')
 			{
-				line[i] = text[i];
-				i++;
+				i = 0;
+				while (text[i] != '\n')
+				{
+					line[i] = text[i];
+					i++;
+				}
+				line[i] = '\n';
+				text = ft_free_line(text);
 			}
-			text = ft_free_line(text);
+			j++;
 		}
 	}
 	else
 		return (NULL);
 	return (line);
 }
-
 /*
 #include <stdio.h>
 #include <fcntl.h>
