c2cbe41ba56827fe26ad2ee66a832e7af0de36f5
Nanelan <151051891+Nanelan@users.noreply.github.com>
2024-01-29 15:43:56 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: crmunoz- <crmunoz-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 10:42:42 by crmunoz-          #+#    #+#             */
-/*   Updated: 2024/01/26 14:11:34 by crmunoz-         ###   ########.fr       */
+/*   Updated: 2024/01/29 15:40:58 by crmunoz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ char	*ft_free_line(char *text)
 	char	*temptext;
 	int		i;
 	int		j;
-	int		len;
 
 	i = 0;
 	j = 0;
+	if (!text)
+		return (NULL);
 	while (text[i] != '\n')
 		i++;
-	len = ft_strlen(text);
-	len = len - (++i);
-	temptext = malloc(sizeof(char) * (len + 1));
+	temptext = (char *)malloc(1 + ft_strlen(text) - i);
 	if (temptext == NULL)
 		return (NULL);
+	i++;
 	while (text[i + j] != '\0')
 	{
 		temptext[j] = text[i + j];
@@ -39,13 +39,14 @@ char	*ft_free_line(char *text)
 	return (temptext);
 }
 
-
 char	*ft_malloc_line(char *text)
 {
 	char	*line;
 	int		i;
 
 	i = 0;
+	if (!text)
+		return (NULL);
 	while (text[i] != '\n')
 		i++;
 	line = malloc(sizeof(char) * (i + 2));
@@ -59,6 +60,28 @@ char	*ft_malloc_line(char *text)
 	}
 	line[i] = text[i];
 	line[++i] = '\0';
+	return (line);
+}
+
+char	*ft_final_line(char *text)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	while (text[i])
+		i++;
+	line = malloc(sizeof(char) * (i + 1));
+	if (line == NULL)
+		return (NULL);
+	i = 0;
+	while (text[i])
+	{
+		line[i] = text[i];
+		i++;
+	}
+	line[i] = '\0';
+	free (text);
 	return (line);
 }
 
@@ -86,13 +109,19 @@ char	*get_next_line(int fd)
 	if ((ft_strchr(text, '\n') == -1) && bytesread > 0)
 		return (get_next_line(fd));
 	else if (bytesread == 0 && text != NULL)
-		return (text);
-	else if (bytesread == 0 && !text)
+	{
+		if (line)
+			free (line);
+		line = ft_final_line(text);
+		text = NULL;
+		return (line);
+	}
+	else if (bytesread == 0 && text == NULL)
 		return (NULL);
 	else
 	{
 		if (line)
-			free(line);
+			free (line);
 		line = ft_malloc_line(text);
 		text = ft_free_line(text);
 		return (line);
@@ -104,9 +133,9 @@ char	*get_next_line(int fd)
 int	main()
 {
 	int fd = open ("hola", O_RDONLY);
-	printf("1. La puta función de los cojones: %s", get_next_line(fd));
-	printf("2. La puta función de los cojones: %s", get_next_line(fd));
-	printf("3. La puta función de los cojones: %s", get_next_line(fd));
+	printf("1. La puta función de los cojones: %s\n", get_next_line(fd));
+	printf("2. La puta función de los cojones: %s\n", get_next_line(fd));
+	printf("3. La puta función de los cojones: %s\n", get_next_line(fd));
 }*/
 
 
